44857fb8bdc41a1ac23117c8c1ba177f2ff2560e
hillce <charles.hill@dtc.ox.ac.uk>
2021-05-10 12:15:44 +0100

    Updated Datasets inheritance

--- datasets.py
+++ datasets.py
@@ -1,6 +1,7 @@
 import os, copy, json, sys, re, warnings
 
 import torch
+from torch.utils import data
 from torch.utils.data import Dataset, DataLoader
 from scipy.io import loadmat
 import numpy as np
@@ -8,115 +9,109 @@ import matplotlib.pyplot as plt
 
 import torchvision.transforms as transforms
 
-class T1_Train_Meta_Dataset(Dataset):
-    """
-    T1 Dataset
-    """
-    def __init__(self,modelDir,size=2000,fileDir="C:/fully_split_data/",t1MapDir="C:/T1_Maps/",load=True,transform=None):
+from meta_function import meta_loading
+
+class Base_Dataset(Dataset):
+
+    def __init__(self,modelDir,fileDir="C:/fully_split_data/",t1MapDir="C:/T1_Maps/",transform=None,size=2000):
+        self.modelDir = modelDir
         self.fileDir = fileDir
         self.t1MapDir = t1MapDir
         self.transform = transform
+        self.size = size
+        self.metaData = meta_loading()
+
+    def get_itm(self,index,dataset):
+
+        inpData = np.load("{}{}_20204_2_0.npy".format(self.fileDir,dataset[index]))
+        inpDataInvTime = np.load("{}{}_20204_2_0_inv_times.npy".format(self.fileDir,dataset[index]))
+        inpMeta = self.metaData[dataset[index]]
+
+        try:
+            outGT = loadmat("{}{}_20204_2_0.mat".format(self.t1MapDir,dataset[index]))['results']
+        except:
+            outGT = loadmat("{}{}_20204_2_0.mat".format(self.t1MapDir,dataset[index]))['x']
+
+        sample = {"Images":inpData,"T1Map":outGT}
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        sample = {"Images":sample["Images"],"T1Map":sample["T1Map"],"InvTime":inpDataInvTime,"eid":dataset[index],"Meta":inpMeta}
+        return sample
+
+    def trim_meta(self,dataset):
+        metaCopy = copy.deepcopy(self.metaData)
+        for k in metaCopy.keys():
+            if k not in dataset.keys():
+                del self.metaData[k]
+
+        del metaCopy
+
+class Train_Meta_Dataset(Base_Dataset):
+
+    def __init__(self,modelDir,size=2000,fileDir="C:/fully_split_data/",t1MapDir="C:/T1_Maps/",load=True,transform=None):
+        Base_Dataset.__init__(self,modelDir,fileDir=fileDir,t1MapDir=t1MapDir,transform=transform,size=size)
 
         if not load:
-            subjList = [x[:7] for x in os.listdir(fileDir) if x.endswith("_2_0.npy") if os.path.isfile("{}{}_20204_2_0.mat".format(t1MapDir,x[:7]))]
-            self.trainSet = np.random.choice(subjList,size)
-            np.save("{}trainSet.npy".format(modelDir),self.trainSet)
+            subjList = [x[:7] for x in os.listdir(self.fileDir) if x.endswith("_2_0.npy") if os.path.isfile("{}{}_20204_2_0.mat".format(self.t1MapDir,x[:7]))]
+            self.trainSet = np.random.choice(subjList,self.size)
+            np.save("{}trainSet.npy".format(self.modelDir),self.trainSet)
         else:
             self.trainSet = np.load("trainSet.npy".format(modelDir))
             np.save("{}trainSet.npy".format(modelDir),self.trainSet)
 
+        self.trim_meta(self.trainSet)
+
     def __getitem__(self, index):
-
-        inpData = np.load("{}{}_20204_2_0.npy".format(self.fileDir,self.trainSet[index]))
-        inpDataInvTime = np.load("{}{}_20204_2_0_inv_times.npy".format(self.fileDir,self.trainSet[index]))
-        try:
-            outGT = loadmat("{}{}_20204_2_0.mat".format(self.t1MapDir,self.trainSet[index]))['results']
-        except:
-            outGT = loadmat("{}{}_20204_2_0.mat".format(self.t1MapDir,self.trainSet[index]))['x']
-
-        sample = {"Images":inpData,"T1Map":outGT}
-
-        if self.transform:
-            sample = self.transform(sample)
-
-        sample = {"Images":sample["Images"],"T1Map":sample["T1Map"],"InvTime":inpDataInvTime,"eid":self.trainSet[index]}
-        return sample
+        return self.get_itm(index,self.trainSet)
 
     def __len__(self):
         return len(self.trainSet)
 
-class T1_Val_Meta_Dataset(Dataset):
-    """
-    T1 Dataset
-    """
-    def __init__(self,modelDir,size=500,fileDir="C:/fully_split_data/",t1MapDir="C:/T1_Maps/",load=True,transform=None):
-        self.fileDir = fileDir
-        self.t1MapDir = t1MapDir
-        self.transform = transform
+class Val_Meta_Dataset(Base_Dataset):
+
+    def __init__(self,modelDir,size=2000,fileDir="C:/fully_split_data/",t1MapDir="C:/T1_Maps/",load=True,transform=None):
+        Base_Dataset.__init__(self,modelDir,fileDir=fileDir,t1MapDir=t1MapDir,transform=transform,size=size)
 
         if not load:
-            subjList = [x[:7] for x in os.listdir(fileDir) if x.endswith("_2_0.npy") if os.path.isfile("{}{}_20204_2_0.mat".format(t1MapDir,x[:7]))]
-            self.trainSet = np.load("{}trainSet.npy".format(modelDir))
+            subjList = [x[:7] for x in os.listdir(self.fileDir) if x.endswith("_2_0.npy") if os.path.isfile("{}{}_20204_2_0.mat".format(self.t1MapDir,x[:7]))]
+            self.trainSet = np.load("{}trainSet.npy".format(self.modelDir))
             subjList = [x for x in subjList if x not in self.trainSet]
-            self.valSet = np.random.choice(subjList,size)
-            np.save("{}valSet.npy".format(modelDir),self.valSet)
+            self.valSet = np.random.choice(subjList,self.size)
+            np.save("{}valSet.npy".format(self.modelDir),self.valSet)
         else:
-            self.valSet = np.load("valSet.npy".format(modelDir))
-            np.save("{}valSet.npy".format(modelDir),self.valSet)
+            self.valSet = np.load("valSet.npy".format(self.modelDir))
+            np.save("{}valSet.npy".format(self.modelDir),self.valSet)
+
+        self.trim_meta(self.valSet)
 
     def __getitem__(self, index):
-
-        inpData = np.load("{}{}_20204_2_0.npy".format(self.fileDir,self.valSet[index]))
-        inpDataInvTime = np.load("{}{}_20204_2_0_inv_times.npy".format(self.fileDir,self.valSet[index]))
-        try:
-            outGT = loadmat("{}{}_20204_2_0.mat".format(self.t1MapDir,self.valSet[index]))['results']
-        except:
-            outGT = loadmat("{}{}_20204_2_0.mat".format(self.t1MapDir,self.valSet[index]))['x']
-
-        sample = {"Images":inpData,"T1Map":outGT}
-
-        if self.transform:
-            sample = self.transform(sample)
-
-        sample = {"Images":sample["Images"],"T1Map":sample["T1Map"],"InvTime":inpDataInvTime,"eid":self.valSet[index]}
-        return sample
+        return self.get_itm(index,self.valSet)
 
     def __len__(self):
         return len(self.valSet)
 
-class T1_Test_Meta_Dataset(Dataset):
-    """
-    T1 Dataset
-    """
-    def __init__(self,modelDir,size=500,fileDir="C:/fully_split_data/",t1MapDir="C:/T1_Maps/",load=True,transform=None):
-        self.fileDir = fileDir
-        self.t1MapDir = t1MapDir
-        self.transform = transform
+class Test_Meta_Dataset(Base_Dataset):
+
+    def __init__(self,modelDir,size=2000,fileDir="C:/fully_split_data/",t1MapDir="C:/T1_Maps/",load=True,transform=None):
+        Base_Dataset.__init__(self,modelDir,fileDir=fileDir,t1MapDir=t1MapDir,transform=transform,size=size)
 
         if not load:
-            subjList = [x[:7] for x in os.listdir(fileDir) if x.endswith("0.npy") if os.path.isfile("{}{}_20204_2_0.mat".format(t1MapDir,x[:7]))]
-            self.trainSet = np.load("{}trainSet.npy".format(modelDir))
-            self.valSet = np.load("{}valSet.npy".format(modelDir))
+            subjList = [x[:7] for x in os.listdir(self.fileDir) if x.endswith("_2_0.npy") if os.path.isfile("{}{}_20204_2_0.mat".format(self.t1MapDir,x[:7]))]
+            self.trainSet = np.load("{}trainSet.npy".format(self.modelDir))
+            self.valSet = np.load("{}valSet.npy".format(self.modelDir))
             subjList = [x for x in subjList if x not in self.trainSet and x not in self.valSet]
-            self.testSet = np.random.choice(subjList,size)
-            np.save("{}testSet.npy".format(modelDir),self.testSet)
+            self.testSet = np.random.choice(subjList,self.size)
+            np.save("{}testSet.npy".format(self.modelDir),self.testSet)
         else:
-            self.testSet = np.load("testSet.npy".format(modelDir))
-            np.save("{}testSet.npy".format(modelDir),self.testSet)
+            self.testSet = np.load("testSet.npy".format(self.modelDir))
+            np.save("{}testSet.npy".format(self.modelDir),self.testSet)
+
+        self.trim_meta(self.testSet)
 
     def __getitem__(self, index):
-
-        inpData = np.load("{}{}_20204_2_0.npy".format(self.fileDir,self.testSet[index]))
-        inpDataInvTime = np.load("{}{}_20204_2_0_inv_times.npy".format(self.fileDir,self.testSet[index]))
-        outGT = loadmat("{}{}_20204_2_0.mat".format(self.t1MapDir,self.testSet[index]))['results']
-
-        sample = {"Images":inpData,"T1Map":outGT}
-
-        if self.transform:
-            sample = self.transform(sample)
-
-        sample = {"Images":sample["Images"],"T1Map":sample["T1Map"],"InvTime":inpDataInvTime,"eid":self.testSet[index]}
-        return sample
+        return self.get_itm(index,self.testSet)
 
     def __len__(self):
         return len(self.testSet)
@@ -196,29 +191,8 @@ def collate_fn(sampleBatch):
     invTimes = [item['InvTime'] for item in sampleBatch]
     invTimes = torch.tensor(invTimes)
 
-    sample = {"Images":inpData,"T1Map":outGT,"InvTime":invTimes,"eid":eid}
+    metaData = [item["Meta"] for item in sampleBatch]
+    metaData = torch.tensor(metaData)
+
+    sample = {"Images":inpData,"T1Map":outGT,"InvTime":invTimes,"eid":eid,"Meta":metaData}
     return sample
-
-if __name__ == "__main__":
-
-    rA = Random_Affine()
-    toT = ToTensor()
-    trnsIn = transforms.Compose([toT,rA])
-
-    dataset = T1_Train_Meta_Dataset(transform=trnsIn)
-    loader = DataLoader(dataset,batch_size=2,shuffle=True,collate_fn=collate_fn,pin_memory=False)
-
-    batch = next(iter(loader))
-    
-    batchImg = batch["Images"].numpy()
-    batchT1 = batch["T1Map"].numpy()
-    for i in range(batchImg.shape[0]):
-        fig,ax = plt.subplots(1,batchImg.shape[1])
-        for j in range(batchImg.shape[1]):
-            ax[j].imshow(batchImg[i,j,:,:])
-
-        plt.figure()
-        plt.imshow(batchT1[i,0,:,:])
-        plt.show()
-
-
