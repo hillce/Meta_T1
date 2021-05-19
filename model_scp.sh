a9b7e47d9f3044505b1c8dd2ba7ddca2262b63d9
hillce <charles.hill@dtc.ox.ac.uk>
2021-05-19 09:25:26 +0100

    vgg test + scp script

---
+++ model_scp.sh
@@ -0,0 +1,3 @@
+scp -r chill@carpc143.cardiov.ox.ac.uk:/home/chill/Meta_T1/models/VGG_16 .\models\
+scp -r chill@carpc143.cardiov.ox.ac.uk:/home/chill/Meta_T1/models/DCGAN_ESQ .\models\
+scp -r chill@carpc143.cardiov.ox.ac.uk:/home/chill/Meta_T1/models/Braided_Net .\models\
